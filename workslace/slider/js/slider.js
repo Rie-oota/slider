3ae68e8e64f6ac8c82501547232bdcc5e4761ffe
Yoshida <sunnri257@gmail.com>
2025-06-15 22:41:26 +0900

    課題修正

--- workslace/slider/js/slider.js
+++ workslace/slider/js/slider.js
@@ -1,57 +1,43 @@
-
 // 表示させたい画像を格納する
-const picture_src = new Array("./image/spring.jpg","./image/summer.jpg","./image/fall.jpg","./image/winter.jpg");
-// 変数pictureNumに-1を代入する
+const picture_src = ["./image/spring.jpg", "./image/summer.jpg", "./image/fall.jpg", "./image/winter.jpg"];
+// 現在のスライドインデックス
 let pictureNum = -1;
-// 画像の切り替えを実行する
-slideshow_timer();
+// タイマーをグローバルで管理
+let timer;
 
-// 画像の切り替えを実行する
-function slideshow_timer(){
-  // 変数pictureNumが3の場合は、pictureNumに0を代入する
-  if (pictureNum == 3){
-    pictureNum = 0;
-  } 
-  // 変数pictureNumが3以外の場合は、pictureNumを1増やす
-  else {
-    pictureNum ++;
-  }
-  // pictureNum番目の画像を表示する
-  document.getElementById("picture").src=picture_src[pictureNum];
-  // 時間間隔を5秒に指定する
-  const timeInterval = 5000;
-  // 画像の切り替えを５秒間隔で実行する
-  const timer = setTimeout("slideshow_timer()",timeInterval); 
+// 共通のスライド切り替えロジック
+function changeSlide(direction) {
+  // インデックスを循環させる
+  pictureNum = (pictureNum + direction + picture_src.length) % picture_src.length;
+  document.getElementById("picture").src = picture_src[pictureNum];
 }
 
-// 時計回りに画像を表示する
-function go_forward(){
-  // 変数pictureNumが3の場合は、pictureNumに0を代入する
-  if (pictureNum == 3) {
-    pictureNum = 0;
-  }
-  // 変数pictureNumが3以外の場合は、pictureNumを1増やす
-  else {
-    pictureNum ++;
-  }
-  // pictureNum番目の画像を表示する
-  document.getElementById("picture").src=picture_src[pictureNum];
-  // タイマーをリセットする
-  clearTimeout(timer);
+// 自動再生機能
+function startSlideshow() {
+  // 5秒間隔で次のスライドを表示
+  timer = setInterval(() => changeSlide(1), 5000);
 }
 
-// 反時計回りに画像を表示する
-function go_back(){
-  // 変数pictureNumが0の場合は、pictureNumに3を代入する
-  if (pictureNum == 0) {
-    pictureNum = 3;
-  }
-  // 変数pictureNumが0以外の場合は、pictureNumを1減らす
-  else {
-    pictureNum --;
-  }
-  // pictureNum番目の画像を表示する
-  document.getElementById("picture").src=picture_src[pictureNum];
-  // タイマーをリセットする
-  clearTimeout(timer);
+// スライドショーを一時停止
+function stopSlideshow() {
+  clearInterval(timer);
 }
+
+// ボタン操作のイベントリスナーを設定
+document.getElementById("prev").addEventListener('click', () => {
+  stopSlideshow(); // 自動再生を一時停止
+  changeSlide(-1); // 前のスライドに変更
+  startSlideshow(); // 再度自動再生を開始
+});
+
+document.getElementById("next").addEventListener('click', () => {
+  stopSlideshow(); // 自動再生を一時停止
+  changeSlide(1); // 次のスライドに変更
+  startSlideshow(); // 再度自動再生を開始
+});
+
+// ページが読み込まれたら自動再生開始
+window.onload = () => {
+  changeSlide(1); // 最初に1枚目を表示
+  startSlideshow(); // 再度自動再生を開始
+};
